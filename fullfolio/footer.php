0a1a06978769e1d1b79316d9174885568cb30f0c
DevonCarlson <Devon@carlsonevonsmbp.wireless.concordia.ca>
2015-05-12 11:28:51 -0400

    message

---
+++ fullfolio/footer.php
@@ -0,0 +1,16 @@
+<?php if ( is_active_sidebar( 'fullfolio-footer')) { ?>
+
+        <!-- footer -->
+        <footer>
+
+            <?php dynamic_sidebar( 'fullfolio-footer' ); ?>
+
+        </footer>
+        <!-- /footer -->
+
+<?php } ?> 
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
