fdc0340cc3d641df16d1999c0ad626e7bc6d048b
DevonCarlson <Devon@CarlsonevonsMBP.wireless.concordia.ca>
2015-05-22 11:53:26 -0400

    dcsdcds

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 
 var videos = [
-	"https://www.youtube.com/watch?v=YuS7iToVsa4#t=2m0s",
-	"https://www.youtube.com/watch?v=wepgRfpjBAQ",
-	"https://www.youtube.com/watch?v=O_YHbodBzy4",
-	"https://www.youtube.com/watch?v=F07zOFLf5rk",
-    "https://www.youtube.com/watch?v=Uhky7pALFSA#t=2m5s",
-    "https://www.youtube.com/watch?v=fnkaI8pkhXs#t=2m40s",
-    "https://www.youtube.com/watch?v=ViLwNTgfNS8",
-    "https://www.youtube.com/watch?v=hDXvtqvXL54",
-    "https://www.youtube.com/watch?v=-SomEwQ6L_s",
-    "https://www.youtube.com/watch?v=BxjMGiN0jJY&feature=youtu.be",
-    "https://www.youtube.com/watch?v=DQ7zStSis_k",
-    "https://www.youtube.com/watch?v=bmxACZd36jA#t=2m0s"
+	"https://www.youtube.com/v/YuS7iToVsa4#t=2m0s",
+	"https://www.youtube.com/v/wepgRfpjBAQ",
+	"https://www.youtube.com/v/O_YHbodBzy4",
+	"https://www.youtube.com/v/F07zOFLf5rk",
+    "https://www.youtube.com/v/Uhky7pALFSA#t=2m5s",
+    "https://www.youtube.com/v/fnkaI8pkhXs#t=2m40s",
+    "https://www.youtube.com/v/ViLwNTgfNS8",
+    "https://www.youtube.com/v/hDXvtqvXL54",
+    "https://www.youtube.com/v/-SomEwQ6L_s",
+    "https://www.youtube.com/v/BxjMGiN0jJY&feature=youtu.be",
+    "https://www.youtube.com/v/DQ7zStSis_k",
+    "https://www.youtube.com/v/bmxACZd36jA#t=2m0s"
 
 
 
