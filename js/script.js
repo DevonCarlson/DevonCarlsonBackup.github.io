5333eb8cde095e5b9be57e8719a79e07026dfb80
DevonCarlson <Devon@CarlsonevonsMBP.wireless.concordia.ca>
2015-07-08 11:57:36 -0400

    all day

--- js/script.js
+++ js/script.js
@@ -2,14 +2,15 @@
 var used_indexes = [];
 
 var videos = [
-	"https://www.youtube.com/v/YuS7iToVsa4&start=120&autoplay=1",
-	"https://www.youtube.com/v/wepgRfpjBAQ&autoplay=1",
-	"https://www.youtube.com/v/O_YHbodBzy4&autoplay=1",
-	"https://www.youtube.com/v/F07zOFLf5rk&autoplay=1",
-	"https://www.youtube.com/watch?v=q8DmGy3inh0&start=2430&autoplay=1",
-    "https://www.youtube.com/v/Uhky7pALFSA&start=125&autoplay=1",
-    "https://www.youtube.com/v/fnkaI8pkhXs&start=160&autoplay=1",
-    "https://www.youtube.com/v/hDXvtqvXL54&autoplay=1"
+"https://www.youtube.com/watch?v=9Q9niHvP0g8&autoplay=1",
+	//"https://www.youtube.com/v/YuS7iToVsa4&start=120&autoplay=1",
+	//"https://www.youtube.com/v/wepgRfpjBAQ&autoplay=1",
+	//"https://www.youtube.com/v/O_YHbodBzy4&autoplay=1",
+	//"https://www.youtube.com/v/F07zOFLf5rk&autoplay=1",
+	//"https://www.youtube.com/watch?v=q8DmGy3inh0&start=2430&autoplay=1",
+   // "https://www.youtube.com/v/Uhky7pALFSA&start=125&autoplay=1",
+  //  "https://www.youtube.com/v/fnkaI8pkhXs&start=160&autoplay=1",
+   // "https://www.youtube.com/v/hDXvtqvXL54&autoplay=1"
 ];
 
 
