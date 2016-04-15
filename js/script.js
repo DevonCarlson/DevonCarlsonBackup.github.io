dc0e7704eb714cc21acc97a349c75dbf5261f62b
DevonCarlson <Devon@carlsonevonsmbp.wireless.concordia.ca>
2016-04-15 13:19:17 -0400

    efdefedf

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ var videos = [
 	"https://www.linkedin.com/in/devoncarlson1"
 ];
 
+var animals = [
+
+	"https://www.google.ca/search?q=teacup+pomeranians&client=safari&rls=en&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj8h6zkkZHMAhWHHD4KHQZMBYIQ_AUIBygB&biw=1280&bih=643",
+	"https://www.google.ca/search?q=teacup+pomeranians&client=safari&rls=en&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj8h6zkkZHMAhWHHD4KHQZMBYIQ_AUIBygB&biw=1280&bih=643#tbm=isch&q=long+haired+chihuahuas"
+];
 
 function randomizeVideo(){
 	var size  = videos.length-1; 
@@ -26,6 +31,22 @@ function randomizeVideo(){
 	  '_blank' // <- This is what makes it open in a new window.
 	);
 }
+
+function randomizeLink(){
+	var size  =  animals.length-1; 
+
+	var index = Math.floor(Math.random()*(size-0+1)+0);
+	
+	if(used_indexes.length == animals.length){
+		used_indexes = [];
+	}
+
+	var animal = document.getElementById("animallink");
+	animal.src = animals[animals[index]];
+
+}
+
+window.onload = randomizeLink();
 
 //"https://www.youtube.com/watch?v=9Q9niHvP0g8autoplay=1"
 //"http://tinyurl.com/o4dwhql"
