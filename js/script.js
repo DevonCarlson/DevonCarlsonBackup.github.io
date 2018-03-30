2f1c9c2a2da25138233ad05cec8a591ae1dc4e96
Devon Carlson <Devon.carlson@me.com>
2018-03-30 17:38:11 -0400

    be
    ded

--- js/script.js
+++ js/script.js
@@ -2,15 +2,22 @@
 var used_indexes = [];
 
 var videos = [
-	"https://www.youtube.com/v/YuS7iToVsa4&start=120&autoplay=1",
-	"https://www.youtube.com/v/wepgRfpjBAQ&autoplay=1",
-	"https://www.youtube.com/v/O_YHbodBzy4&autoplay=1",
-	"https://www.youtube.com/v/F07zOFLf5rk&autoplay=1",
-    "https://www.youtube.com/v/Uhky7pALFSA&start=125&autoplay=1",
-    "https://www.youtube.com/v/fnkaI8pkhXs&start=160&autoplay=1",
-    "https://www.youtube.com/v/hDXvtqvXL54&autoplay=1"
+
+	"https://www.linkedin.com/in/devoncarlson1"
 ];
 
+var animals = [
+
+"https://www.google.ca/search?q=long+haired+chihuahua&client=safari&rls=en&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj6kdTvoJHMAhULnIMKHdIlB7AQ_AUIBygB&biw=1280&bih=643",
+	"https://www.google.ca/search?q=teacup+pomeranians&client=safari&rls=en&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj8h6zkkZHMAhWHHD4KHQZMBYIQ_AUIBygB&biw=1280&bih=643",
+	"https://www.google.ca/search?q=baby+dik+dik&client=safari&rls=en&tbm=isch&tbo=u&source=univ&sa=X&ved=0ahUKEwiA89LsoJHMAhUquoMKHXVZBS8QsAQIGw&biw=1280&bih=643",
+	"https://www.google.ca/search?q=long+haired+chihuahua&client=safari&rls=en&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj6kdTvoJHMAhULnIMKHdIlB7AQ_AUIBygB&biw=1280&bih=643#tbm=isch&q=baby+fox",
+	"https://www.google.ca/search?q=long+haired+chihuahua&client=safari&rls=en&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj6kdTvoJHMAhULnIMKHdIlB7AQ_AUIBygB&biw=1280&bih=643#tbm=isch&q=baby+goat",
+	"https://www.google.ca/search?q=long+haired+chihuahua&client=safari&rls=en&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj6kdTvoJHMAhULnIMKHdIlB7AQ_AUIBygB&biw=1280&bih=643#tbm=isch&q=baby+beluga+whale",
+	"https://www.google.ca/search?q=long+haired+chihuahua&client=safari&rls=en&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj6kdTvoJHMAhULnIMKHdIlB7AQ_AUIBygB&biw=1280&bih=643#tbm=isch&q=baby+puffer+fish",
+	"https://www.google.ca/search?q=long+haired+chihuahua&client=safari&rls=en&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj6kdTvoJHMAhULnIMKHdIlB7AQ_AUIBygB&biw=1280&bih=643#tbm=isch&q=baby+raccoon"
+	
+];
 
 function randomizeVideo(){
 	var size  = videos.length-1; 
@@ -31,3 +38,23 @@ function randomizeVideo(){
 	  '_blank' // <- This is what makes it open in a new window.
 	);
 }
+
+function randomizeLink(){
+	var size  =  animals.length-1; 
+
+	var index = Math.floor(Math.random()*(size-0+1)+0);
+	
+	if(used_indexes.length == animals.length){
+		used_indexes = [];
+	}
+
+	var animal = document.getElementById("animallink");
+	animal.href = animals[index];
+	console.log(animals[index]);
+
+}
+
+window.onload = randomizeLink();
+
+//"https://www.youtube.com/watch?v=9Q9niHvP0g8autoplay=1"
+//"http://tinyurl.com/o4dwhql"
